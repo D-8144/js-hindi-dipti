97080996af7ede48c059722c970039c6c6952b0e
D-8144 <diptiguin217aa@gmail.com>
2025-12-11 07:22:25 +0000

    Strings

--- 01basics/05_strings.js
+++ 01basics/05_strings.js
@@ -1,3 +1,10 @@
 const name = "Dipti"
 const age = 20
 console.log(`My name is ${name} & my age is ${age}`);
+
+
+const newName = new String ('Abhisek')
+console.log(newName[0]);
+console.log(newName.__proto__);
+
+console.log(newName.length);
